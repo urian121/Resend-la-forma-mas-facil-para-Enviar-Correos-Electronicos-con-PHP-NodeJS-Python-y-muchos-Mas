e5a3f7725d2c78d6c571a804507f37020f559a0c
URIAN VIERA <urian1213viera@gmail.com>
2023-10-25 15:11:31 -0500

    enviando email con resend desde nodejs

---
+++ Resend-NodeJS/index.js
@@ -0,0 +1,19 @@
+import { Resend } from "resend";
+const resend = new Resend("re_gQYDfsg9_DY6dVbrbi1yM6nTHSyuSKNkc");
+
+const sendEmail = async () => {
+  try {
+    const data = await resend.emails.send({
+      from: "NodeJS <node@resend.dev>",
+      to: ["urian1213viera@gmail.com"],
+      subject: "Email desde NodeJS",
+      html: "<p>Hola Comunidad, enviando email desde NodeJS con Resend",
+    });
+
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+sendEmail();
--- Resend-PHP/email.php
+++ Resend-PHP/email.php
@@ -1,7 +1,7 @@
 <?php
 if (($_SERVER["REQUEST_METHOD"] == "POST")) {
     require __DIR__ . './vendor/autoload.php';
-    //$resend = Resend::client('re_gQYDfsg9_DY6dVbrbi1yM6nTHSyuSKNkc');
+    $resend = Resend::client('re_gQYDfsg9_DY6dVbrbi1yM6nTHSyuSKNkc');
 
     $nombre_cliente       = $_POST['nombre_cliente'];
     $email_cliente        = $_POST['email_cliente'];
@@ -9,10 +9,19 @@ if (($_SERVER["REQUEST_METHOD"] == "POST")) {
     $terminos = $_POST['terminos'] ? 1 : 0;
 
 
-    $resend->emails->send([
-        'from' => 'Acme <onboarding@resend.dev>',
-        'to' => ['urian1213vieragmail.com'],
-        'subject' => 'hello world',
-        'html' => '<strong>it works!</strong>',
-    ]);
+    try {
+        $result = $resend->emails->send([
+            'from' => 'PHP <php@resend.dev>',
+            'to' => ['urian1213viera@gmail.com'],
+            'subject' => 'Hola comunidad WebDeveloper ',
+            'html' => '<p>El cliente  <strong>' . $nombre_cliente . '</strong> con email ' . $email_cliente . ' le envia este mensaje </p>
+            <strong>' . $mensaje_cliente . '</strong>',
+        ]);
+    } catch (\Exception $e) {
+        exit('Error: ' . $e->getMessage());
+    }
+
+    echo $result->toJson();
+    header("Location: index.html");
+    die();
 }
