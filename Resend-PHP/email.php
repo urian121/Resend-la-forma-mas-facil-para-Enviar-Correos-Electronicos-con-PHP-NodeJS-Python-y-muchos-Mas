6dd5930cf75bcea0574729ea9c950433f75563dc
URIAN VIERA <urian1213viera@gmail.com>
2023-10-25 18:46:15 -0500

    resend email funcionado con PHP

--- Resend-PHP/email.php
+++ Resend-PHP/email.php
@@ -1,6 +1,10 @@
 <?php
 if (($_SERVER["REQUEST_METHOD"] == "POST")) {
-    require __DIR__ . './vendor/autoload.php';
+    //Para windows el path cambia
+    //require __DIR__ . './vendor/autoload.php';
+
+    require __DIR__ . '/vendor/autoload.php';
+
     $resend = Resend::client('re_gQYDfsg9_DY6dVbrbi1yM6nTHSyuSKNkc');
 
     $nombre_cliente       = $_POST['nombre_cliente'];
